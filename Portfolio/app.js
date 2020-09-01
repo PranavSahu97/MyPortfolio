b43cf2a0aa82db1484ac37ad59e58fac5342169a
Pranav S. Sahu <sahupranav97@gmail.com>
2020-08-31 20:14:40 -0700

    Icon fixed

--- Portfolio/app.js
+++ Portfolio/app.js
@@ -7,12 +7,37 @@ hamburger.addEventListener('click', () => {
     mobile_menu.classList.toggle('active');
 })
 
-document.addEventListener('scroll', () => {
-    var scroll_position = window.scrollY;
-    if (scroll_position > 200) {
-        header.style.backgroundColor = "#272727";
-    }
-    else {
-        header.style.backgroundColor = 'transparent';
-    }
-})
+
+// var button = document.getElementById('hamburger');
+//     // span = button.getElementsByTagName('span')[0];
+
+// button.onclick =  function() {
+//   hamburger.classList.toggle('hamburger-menu-button-close');
+// };
+
+// $('#hamburger').on('click', toggleOnClass);
+
+// function toggleOnClass(event) {
+//   var toggleElementId = '#' + $(this).data('toggle'),
+//   element = $(toggleElementId);
+
+//   element.toggleClass('on');
+
+// }
+
+// // close hamburger menu after click a
+// $( '.navlist ul li a' ).on("click", function(){
+//   $('hamburger').click();
+// });
+
+
+
+// document.addEventListener('scroll', () => {
+//     var scroll_position = window.scrollY;
+//     if (scroll_position > 200) {
+//         header.style.backgroundColor = "#272727";
+//     }
+//     else {
+//         header.style.backgroundColor = 'transparent';
+//     }
+// })
